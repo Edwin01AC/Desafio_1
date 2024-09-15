395c60f8ea81085cecc2352123250da821ab8dd7
Edwin01AC <edwin.piedrahita1@udea.edu.co>
2024-09-16 04:47:16 +0530

    Se agrego la funcion medirAmplitud al main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include <Adafruit_LiquidCrystal.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+Adafruit_LiquidCrystal lcd(0);
+const int signalPin = A0;
+const int sampleSize = 100;
+
+float medirAmplitud(int *muestras, int tamano) {
+    int maxVal = 0;
+    for (int i = 0; i < tamano; i++) {
+        if (muestras[i] > maxVal) {
+            maxVal = muestras[i];
+        }
+    }
+    return (maxVal * (5.0 / 1023.0));
 }
